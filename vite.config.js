a2a18ca3c8bc7ff7a7f1e5a9d4c512626480d300
Wally Wallace <jrvw001@gmail.com>
2025-04-04 11:53:09 -0600

    Add cypress

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    extensions: ['.js', '.jsx'],  // Ensures Vite resolves JSX files correctly
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/, // Ensures ESBuild processes JSX files
+    exclude: [],
+  },
+});
