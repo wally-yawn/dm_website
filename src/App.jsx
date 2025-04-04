1271e6eed4be3cd0d1ebead9c147f833545a3431
Wally Wallace <jrvw001@gmail.com>
2025-04-04 12:35:51 -0600

    Update main page to show navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 // import { useState } from 'react'
 import './App.css'
 import MainPage from './MainPage/MainPage'
+import { Routes, Route, NavLink} from 'react-router-dom';
+import BandContainer from './BandContainer/BandContainer';
+import SongsContainer from './SongsContainer/SongsContainer';
+import ShowsContainer from './ShowsContainer/ShowsContainer';
 
 function App() {
 
@@ -9,9 +13,17 @@ function App() {
       <header>
         <h1>The Deceitful Mind</h1>
       </header>
-      <section>
-        <MainPage/>
-      </section>
+      <nav>
+        <NavLink to="/shows" className="nav-element">Shows</NavLink>
+        <NavLink to="/theband" className="nav-element">The Band</NavLink>
+        <NavLink to="/songs" className="nav-element">Songs</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<MainPage />}/>
+        <Route path="/theband" element={<BandContainer />}/>
+        <Route path="/songs" element={<SongsContainer />}/>
+        <Route path="/shows" element={<ShowsContainer />}/>
+      </Routes>
     </>
   )
 }
---
+++ src/SongsContainer/SongsContainer.jsx
@@ -0,0 +1,9 @@
+import "./SongsContainer.css";
+
+function SongsContainer(){
+  return(
+    <h2>This is the songs container</h2>
+  )
+}
+
+export default SongsContainer;
