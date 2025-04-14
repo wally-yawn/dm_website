43e29c555544e2b31eba311bdcda92455bb9ebfd
Wally Wallace <jrvw001@gmail.com>
2025-04-14 11:32:14 -0600

    Begin to add cypress tests

--- cypress/e2e/nav.cy.js
+++ cypress/e2e/nav.cy.js
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('navigation', () => {
   it('displays the main page', () => {
     cy.visit('/', { timeout: 10000 });
     cy.get('h1').contains('The Deceitful Mind');
