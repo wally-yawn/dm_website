848f699a976cea3b8e86bb84519440566e5bc5d5
Wally Wallace <jrvw001@gmail.com>
2025-07-01 15:07:30 -0600

    Add band members to container

--- src/BandContainer/BandContainer.jsx
+++ src/BandContainer/BandContainer.jsx
@@ -1,8 +1,28 @@
 import "./BandContainer.css";
+import bandMembers from '../data/bandmembers.json'
+import { useState } from "react"
+import BandMembers from "../BandMembers/BandMembers";
 
 function BandContainer(){
+  
+  const[allBandMembers, setbandMembers] = useState(bandMembers)
+
+  const dmBandMembers = allBandMembers.map((member, index) => {
+    return (
+      <BandMembers
+      id={index}
+      key={index}
+      name={member.name}
+      instrument={member.instrument}
+      />
+    )
+  })
+
   return(
-  <h2>This is the band container</h2>
+    <section className="container">
+      <h2>This is the band container</h2>
+      {dmBandMembers}
+    </section>
   )
 }
 
