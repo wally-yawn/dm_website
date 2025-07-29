7c3bc4371e87419ee7177848c262fda76ab0decd
Wally Wallace <jrvw001@gmail.com>
2025-07-29 16:47:45 -0600

    More css on the songs page

--- src/Songs/Songs.jsx
+++ src/Songs/Songs.jsx
@@ -4,11 +4,13 @@ import spotifyLogo from '../assets/Spotify_Primary_Logo_RGB_Green.png'
 function Songs({title, img, spotify, apple, amazon, tidal, youtube}){
   return (
     <div className="card">
-      <h3>{title}</h3>
-      <span className="links">
-        {/*make this an actual list*/}
-        <a href = {spotify} target="blank" rel="noopener noreferrer"><img src={spotifyLogo} alt="spotify logo" aria-hidden="true" className="icon" /></a>
-      </span>
+      <h2>{title}</h2>
+      <ul className="links">
+        <li><a href = {spotify} target="blank" rel="noopener noreferrer"><img src={spotifyLogo} alt="spotify logo" aria-hidden="true" className="icon" /></a></li>
+        <li> {apple} </li>
+        <li> {amazon} </li>
+        <li> {tidal} </li>
+      </ul>
       <div className="video">
         <iframe src= {youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
