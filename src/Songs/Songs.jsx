a8d21c176bd048dfa1ad330417e8a4a3bacdb43d
Wally Wallace <jrvw001@gmail.com>
2025-07-29 16:36:55 -0600

    More css on embedded youtube videos

--- src/Songs/Songs.jsx
+++ src/Songs/Songs.jsx
@@ -6,9 +6,12 @@ function Songs({title, img, spotify, apple, amazon, tidal, youtube}){
     <div className="card">
       <h3>{title}</h3>
       <span className="links">
+        {/*make this an actual list*/}
         <a href = {spotify} target="blank" rel="noopener noreferrer"><img src={spotifyLogo} alt="spotify logo" aria-hidden="true" className="icon" /></a>
       </span>
-      <iframe width="560" height="315" src= {youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <div className="video">
+        <iframe src= {youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
     </div>
   )
 }
