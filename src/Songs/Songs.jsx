88d0a463b54eb30657196e7ce449087a995d5456
Wally Wallace <jrvw001@gmail.com>
2025-07-29 18:33:19 -0600

    resolve test issues and add more links

--- src/Songs/Songs.jsx
+++ src/Songs/Songs.jsx
@@ -6,20 +6,25 @@ import tidalLogo from '../assets/tidallogo.jpeg'
 
 function Songs({title, img, spotify, apple, amazon, tidal, youtube}){
   
-  const isTest = import.meta.env.MODE === 'test' || Cypress?.env?.('NO_EMBEDS');
+  // remove the embedded youtube during tests
+  const noEmbedsFlag =
+    globalThis.Cypress?.env?.('NO_EMBEDS') ||
+    import.meta.env.VITE_NO_EMBEDS === 'true';
+
+  const isTest = import.meta.env.MODE === 'test' || noEmbedsFlag;
   
   return (
     <div className="card">
       <h2>{title}</h2>
       <ul className="links">
         <li><a href = {spotify} target="blank" rel="noopener noreferrer"><img src={spotifyLogo} alt="spotify logo" aria-hidden="true" className="icon" /></a></li>
-        {/* <li><a href = {apple} target="blank" rel="noopener noreferrer"><img src={appleLogo} alt="spotify logo" aria-hidden="true" className="icon" /></a></li>
-        <li><a href = {amazon} target="blank" rel="noopener noreferrer"><img src={amazonLogo} alt="spotify logo" aria-hidden="true" className="icon" /></a></li>
-        <li><a href = {tidal} target="blank" rel="noopener noreferrer"><img src={tidalLogo} alt="spotify logo" aria-hidden="true" className="icon" /></a></li> */}
+        <li><a href = {apple} target="blank" rel="noopener noreferrer"><img src={appleLogo} alt="spotify logo" aria-hidden="true" className="icon" /></a></li>
+        {/* <li><a href = {amazon} target="blank" rel="noopener noreferrer"><img src={amazonLogo} alt="spotify logo" aria-hidden="true" className="icon" /></a></li> */}
+        {/* <li><a href = {tidal} target="blank" rel="noopener noreferrer"><img src={tidalLogo} alt="spotify logo" aria-hidden="true" className="icon" /></a></li> */}
       </ul>
       {!isTest && (
         <div className="video">
-          <iframe src= {youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe src= {youtube} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
       )}
     </div>
