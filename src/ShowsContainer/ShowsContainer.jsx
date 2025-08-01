a97e3e147933af425ffd56d08fd643362531683f
Wally Wallace <jrvw001@gmail.com>
2025-08-01 12:16:14 -0600

    Seperating upcoming and previous shows

--- src/ShowsContainer/ShowsContainer.jsx
+++ src/ShowsContainer/ShowsContainer.jsx
@@ -5,11 +5,12 @@ import shows from '../data/shows.json';
 
 function ShowsContainer(){
 
-  let allDMShows = null;
+  let upcomingDMShows = null;
+  let pastDMShows = null;
   
   const today = new Date();
   
-  const [allShows, setAllShows] = useState({
+  const [upcomingShows, setUpcomingShows] = useState({
     shows: shows.filter(show => {
       const showDate = new Date(`${show.date} 00:00:00`);
       today.setHours(0,0,0,0);
@@ -21,8 +22,20 @@ function ShowsContainer(){
     })
   })
 
-  if (allShows?.shows?.length > 0){
-    allDMShows = allShows.shows.map((show, index) => {
+  const [pastShows, setPastShows] = useState({
+    shows: shows.filter(show => {
+      const showDate = new Date(`${show.date} 00:00:00`);
+      today.setHours(0,0,0,0);
+      return showDate <= today;
+    }).sort((a,b) => {
+      const dateA = new Date(`${a.date} 00:00:00`);
+      const dateB = new Date(`${b.date} 00:00:00`);
+      return dateA - dateB;
+    })
+  })
+
+  if (upcomingShows?.shows?.length > 0){
+    upcomingDMShows = upcomingShows.shows.map((show, index) => {
       return (
         <Shows
           id={index}
@@ -38,14 +51,33 @@ function ShowsContainer(){
     })
   }
 
+    if (pastShows?.shows?.length > 0){
+    pastDMShows = pastShows.shows.map((show, index) => {
+      return (
+        <Shows
+          id={index}
+          title={show.title}
+          key={index}
+          location={show.location}
+          date={show.date}
+          time={show.time}
+          cover={show.cover}
+          otherbands={show.otherbands}
+        />
+      )
+    })
+  }
   return(
-    <section className="container">
-      <h2 className="sub-header">This is the shows container</h2>
-      {allDMShows === null ? (
-        <p>No upcoming shows, check back soon!</p> 
+    <section className="container shows-container">
+      {upcomingDMShows === null ? (
+        <p>No upcoming shows, check back soon and check out previous show videos below!</p> 
       ) : (
-        <div className="show-cards">{allDMShows}</div>
+        <div className="show-cards">{upcomingDMShows}</div>
       )}
+      <h2>Past Shows!</h2>
+      {console.log(`past shows: `, pastShows)}
+      {console.log(`past DMshows: `, pastDMShows)}
+      <div className="show-cards">{pastDMShows}</div>
     </section>
   )
 }
