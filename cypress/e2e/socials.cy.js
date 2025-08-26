0f8f583d0d092158400b440dcecaa03c88deb2bb
Wally Wallace <jrvw001@gmail.com>
2025-08-26 12:57:18 -0600

    Add lots of css for responsive design

--- cypress/e2e/socials.cy.js
+++ cypress/e2e/socials.cy.js
@@ -3,4 +3,14 @@ describe('social banner', () => {
   beforeEach(() => {
     cy.visit('/', { timeout: 10000 });
   })
+
+  it('displays all the social links', () => {
+    cy.get('.banner-socials a').should('have.length',6)
+    .get('a').eq(0).should('have.attr', 'href', 'https://www.instagram.com/thedeceitfulmind/')
+    .get('a').eq(1).should('have.attr', 'href', 'https://www.facebook.com/TheDeceitfulMind/')
+    .get('a').eq(2).should('have.attr', 'href', 'https://www.youtube.com/channel/UCDJiZDVS2wHexkZvj0kirgQ')
+    .get('a').eq(3).should('have.attr', 'href', 'https://open.spotify.com/artist/1NRdg3EZ3RIQkrUS52eaI6')
+    .get('a').eq(4).should('have.attr', 'href', 'https://music.apple.com/us/artist/the-deceitful-mind/1149643171')
+    .get('a').eq(5).should('have.attr', 'href', 'https://www.amazon.com/music/player/artists/B01LEGQGNI/the-deceitful-mind')
+  })
 })
