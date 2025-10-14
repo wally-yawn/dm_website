2ec5f858910632d016051e2f444d364c239b3f45
Wally Wallace <jrvw001@gmail.com>
2025-10-14 12:12:38 -0600

    add some assets and a little css

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -7,9 +7,12 @@ function Home({ upcomingShows, youtubeEmbed }) {
       <section className="upcoming-shows">
         <h2>Upcoming Shows</h2>
         {upcomingShows.length < 1 ? (
-          <p>
-            No upcoming shows, check back soon! In the meantime check out some videos from our <a href="/pastshows">past shows</a> and our <a href="/songs">released music!</a>
-          </p>
+          <section className="card">
+            <p>
+              No upcoming shows, check back soon! In the meantime check out some videos from our <a href="/pastshows">past shows</a> and our <a href="/songs">released music!</a>
+            </p>
+        </section>
+
         ) : (
           <section className="container shows-container">
             <UpcomingShowsContainer shows={upcomingShows} />
@@ -20,7 +23,7 @@ function Home({ upcomingShows, youtubeEmbed }) {
         <h2>News</h2>
         <section className="card">
           <p>We're hard at work recording new music! Release date announcements coming soon!</p>
-          <img className="news-img" src='../assets/jess_recording.png'></img>
+          <img className="news-img" src='../assets/jess_recording.jpeg'></img>
         </section>
       </section>
     </section>
